ede5f1bcb9662aeb3e480fe78b110b242f8369d6
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-04-26 15:23:22 +0100

    Add basic Cookie Consent API

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,10 @@ module.exports = function (eleventyConfig) {
     { "./node_modules/govuk-frontend/govuk/all.js": "scripts/govuk.js" }
   );
 
+  eleventyConfig.addPassthroughCopy(
+    { "./node_modules/@alphagov/consent-api/client/src/consent.js": "scripts/consent.js" }
+  );
+
   eleventyConfig.addPassthroughCopy("./src/scripts");
   eleventyConfig.addPassthroughCopy("./src/styles");
   eleventyConfig.addPassthroughCopy("./src/assets");
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,25 @@
 window.onload = function (event) {
   console.log("Site loaded...")
+
+  var acceptAllCookies = JSON.stringify({
+    "essential": true,
+    "campaigns": false,
+    "settings": false,
+    "usage": false
+  })
+
+  window.Consent.setStatus(
+    acceptAllCookies,
+    (status) => {
+      console.log("Consent status successfully updated to", status)
+    }
+  )
+
+  window.Consent.onStatusLoaded((status) => {
+    console.log("Consent Status:")
+    console.log(`- Essential cookies (${status.essential})`)
+    console.log(`- Campaign cookies (${status.campaigns})`)
+    console.log(`- Settings cookies (${status.settings})`)
+    console.log(`- Usage cookies (${status.usage})`)
+  })
 }
